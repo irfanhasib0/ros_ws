8a5ca9e701090995f8e237946fa65fedb522c095
irfanhasib0 <irfanhasib.me@gmail.com>
2019-07-15 20:22:40 +0600

    added object 2D moving code

--- ikyne.py
+++ ikyne.py
@@ -1,56 +1,72 @@
 import numpy as np
+class kinematics():
+	def __init__(self,l1,l2,l3,l4):
+		self.l1=l1
+		self.l2=l2
+		self.l3=l3
+		self.l4=l4
+	def fkine(self,anga,angb,angc):
+			self.l1=.4
+			self.l2=1
+			self.l3=1
+			self.l4=.4
+			px=self.l2*np.sin(angb)+self.l3*np.sin(angb+anga)
+			py=self.l2*np.cos(anga)+ self.l3*np.cos(angb+angc)
+			xx=(px+self.l4)*np.cos(anga)
+			yy=(px+self.l4)*np.sin(anga)
+			zz=py+self.l1
+			return xx,yy
+	def ikyne(self,posx,posy,posz):
+	 y1=0
+	 self.l1=10
+	 self.l2=10
+	 x1=np.sqrt(posx*posx+posz*posz+posy*posy);
 
-def get_angle(posx,posy,posz):
- y1=0
- l1=10
- l2=10
- x1=np.sqrt(posx*posx+posz*posz+posy*posy);
+	 #if x1>25 or x1<5:
+	   #return
+	 
+	 a=(float)((x1*x1)+(y1*y1));
 
- #if x1>25 or x1<5:
-   #return
- 
- a=(float)((x1*x1)+(y1*y1));
+	 b=l1*l1+l2*l2;
+	 c=l1*l2;
+	 
+	 cos_b=(a-b)/(float)(2*c);
+	 sin_b=np.sqrt(1-cos_b*cos_b);
 
- b=l1*l1+l2*l2;
- c=l1*l2;
- 
- cos_b=(a-b)/(float)(2*c);
- sin_b=np.sqrt(1-cos_b*cos_b);
+	 angb=np.arccos(cos_b);
+	 #angb=(angb*180)/3.1415;
+	 angb=angb;
+	 
+	 
+	 k1=l1+l2*cos_b;
+	 k2=l2*sin_b;
+	 
+	 anga=np.arctan(y1/float(x1))-np.arctan(k2/float(k1));
+	 anga=anga+np.arctan(float(posy/float(x1)));
+	 if anga<0:
+	  anga=-1*anga;
 
- angb=np.arccos(cos_b);
- #angb=(angb*180)/3.1415;
- angb=angb;
- 
- 
- k1=l1+l2*cos_b;
- k2=l2*sin_b;
- 
- anga=np.arctan(y1/float(x1))-np.arctan(k2/float(k1));
- anga=anga+np.arctan(float(posy/float(x1)));
- if anga<0:
-  anga=-1*anga;
+	 
+	  
+	 anga=anga+np.arcsin(posy/float(x1));
+	 
+	 #anga=(anga*180)/3.1416;
 
- 
-  
- anga=anga+np.arcsin(posy/float(x1));
- 
- #anga=(anga*180)/3.1416;
-
- angc=np.arcsin(posz/float(x1));
- #angc=(angc*180)/3.1416;
- angd=angb-anga;
- angd=angd-90;
- 
+	 angc=np.arcsin(posz/float(x1));
+	 #angc=(angc*180)/3.1416;
+	 angd=angb-anga;
+	 angd=angd-90;
+	 
 
 
- p=anga;
+	 p=anga;
 
- 
- q=angb;
- anga=np.rad2deg(anga)
- angb=np.rad2deg(angb)
- angc=np.rad2deg(angc)
- return anga,angb,angc,angd
+	 
+	 q=angb;
+	 anga=np.rad2deg(anga)
+	 angb=np.rad2deg(angb)
+	 angc=np.rad2deg(angc)
+	 return anga,angb,angc,angd
 
 def valc(l):
  v=int(np.cos(l)*100)
--- test_move.py
+++ test_move.py
@@ -4,11 +4,14 @@ import roslib  #roslib.load_manifest('ur_driver')
 import rospy
 import actionlib
 from control_msgs.msg import *
+from sensor_msgs.msg import Image ,LaserScan, JointState
 from trajectory_msgs.msg import *
 from std_msgs.msg import Float64
+from ikyne import kinematics
 
 JOINT_NAMES = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint',
                'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']
+ur_kin=kinematics(1,1,1,1)
 Q1 = [2.2,0,-1.57,0,0,0]
 Q2 = [1.5,0,-1.57,0,0,0]
 Q3 = [1.5,-0.2,-1.57,0,0,0]
@@ -22,10 +25,23 @@ client = None
 #g = FollowJointTrajectoryGoal()
 traj = JointTrajectory()
 traj.joint_names = JOINT_NAMES
-
+fb_joints=[]
+fb_names=[]
+def cb_joint_states(data):
+        global fb_joints,fb_names
+        #print('received')
+        fb_joints=list(data.position)
+        fb_names=list(data.name)
 rospy.init_node("test_move", anonymous=True, disable_signals=True)
 pub0 = rospy.Publisher('/arm_controller/command', JointTrajectory, queue_size=10)
-pub1 = rospy.Publisher('/linx_controller/command', Float64, queue_size=10)
+box1_x = rospy.Publisher('/box1_linx_controller/command', Float64, queue_size=10)
+box1_y = rospy.Publisher('/box1_liny_controller/command', Float64, queue_size=10)
+def box1_pub(x,y):
+	box1_x.publish(x)
+	box1_y.publish(y)
+rospy.Subscriber("joint_states",JointState, cb_joint_states)
+
+
 rate = rospy.Rate(10)
 anga=0
 angb=0
@@ -33,12 +49,31 @@ angc=0
 angd=0
 ange=0
 angf=0
+#['elbow_joint', 'linx_joint', 'liny_joint', 'shoulder_lift_joint', 'shoulder_pan_joint', 'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']
+fb_anga=0
+fb_angb=0
+fb_angc=0
+fb_linx=0
+fb_liny=0
+
 while(True):
 	anga+=0.1
 
 	if anga>=1.5:
 		anga=0
 	Q1=[anga,angb,angc,angd,ange,angf]
+	try:
+		fb_anga=fb_joints[fb_names.index('shoulder_pan_joint')]
+		fb_angb=fb_joints[fb_names.index('shoulder_lift_joint')]
+		fb_angc=fb_joints[fb_names.index('elbow_joint')]
+		fb_linx=fb_joints[fb_names.index('box1_linx_joint')]
+		fb_liny=fb_joints[fb_names.index('box1_liny_joint')]
+	except:0
+	#print('fb_anga',fb_anga)
+	xx,yy=ur_kin.fkine(fb_anga,fb_angb,fb_angc)
+	print(fb_anga,fb_angb,fb_angc,fb_linx,fb_liny)
+	print(xx,yy)
+	
 	if anga!=0:
 		traj.points = [
 			JointTrajectoryPoint(positions=Q1, velocities=[0]*6, time_from_start=rospy.Duration(0.2))
@@ -49,7 +84,7 @@ while(True):
 			JointTrajectoryPoint(positions=Q1, velocities=[0]*6, time_from_start=rospy.Duration(1.0))
 			#JointTrajectoryPoint(positions=Q2, velocities=[0]*6, time_from_start=rospy.Duration(3.0)),
 			]
-	pub0.publish(traj)
-	pub1.publish(2)
+	#pub0.publish(traj)
+	box1_pub(1,1)
 	rate.sleep()
 	
